bbf744ea51c8b6507dc2d8ca34c19ae707c1920c
John Holler <johnholl@umich.edu>
2022-05-20 22:39:23 -0400

    restyle mainpage
    make mainpage more mobile friendly, extract css to style files.

--- src/components/mainpage/MainPage.js
+++ src/components/mainpage/MainPage.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import PageListing from './PageListing';
-import {Link} from "react-router-dom";
-
+import './styles.css';
 
 export default function MainPage() {
-
     return (
-        <div style={{display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column', paddingTop:50}}>
-            <div style={{width: 1200, minHeight:"100vh", borderStyle:"solid", borderColor:"black", borderRadius:"10px", padding:"10px", backgroundColor:"#CBD7E9", fontSize:"1.5rem", fontFamily:"Andale Mono, monospace"}}>
-                <h1>experiments</h1>
+        <div style={{display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column'}}>
+            <div className="listing-container">
+                <h2>experimentJS</h2>
+                <p>Learn new algorithms through visual programming.</p>
                 <ul className="nav" style={{listStyleType:"none"}}>
                     <PageListing title="list sorting" location="/sorting" img="static_sorting.png" anim="sorting.gif"/>
                     <PageListing title="maze navigation" location="/maze" img="maze_static.png" anim="maze.gif"/>
-                    <PageListing title="graph visual" location="/graph" img="graph_static.png" anim="graph.gif"/>
+                    <PageListing title="graph moving" location="/graph" img="graph_static.png" anim="graph.gif"/>
                     <PageListing title="match tiles" location="/colorpuzzle" img="colorpuzzle_static.png" anim="colorpuzzle.gif"/>
                     <PageListing title="heat simulation" location="/heatrod" img="rod.png" anim="rod.gif"/>
                 </ul>
--- src/components/mainpage/PageListing.js
+++ src/components/mainpage/PageListing.js
@@ -6,7 +6,7 @@ export default function PageListing(props) {
     return(
         <li>
             <Link to={props.location}>
-                <div className="card">
+                <div className="card col-12">
                     <img className="static" src={props.img} alt="Avatar" style={{width:"300px", height:"250px", objectFit:"cover"}}/>
                     <img src={props.anim} alt="Avatar" style={{width:"300px", height:"250px"}}/>
                     <div className="container">
@@ -17,3 +17,26 @@ export default function PageListing(props) {
         </li>
     )
 }
+
+
+
+// import React from 'react';
+// import {Link} from "react-router-dom";
+// import './styles.css';
+
+// export default function PageListing(props) {
+
+//     return(
+//         <li>
+//             <Link to={props.location}>
+//                 <div className="card col-12">
+//                     <img className="static" src={props.img} alt="Avatar" style={{height:"90%", width:"90%"}}/>
+//                     <img src={props.anim} alt="Avatar" style={{height:"100%", width:"100%"}}/>
+//                     {/* <div className="container">
+//                         <h4><b>{props.title}</b></h4>
+//                     </div> */}
+//                 </div>
+//             </Link>
+//         </li>
+//     )
+// }
